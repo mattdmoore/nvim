ee85b7ec84ba759f273dceec8f409379f008c399
Matt Moore <matt.d.moore@outlook.com>
2024-08-04 21:45:58 +0200

    Move mapping to toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,4 +3,21 @@ return {
   opts = {
     shell = "fish",
   },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>ts"] = {
+            function()
+              if vim.fn.executable "spotify_player" then
+                require("astrocore").toggle_term_cmd { cmd = "spotify_player", direction = "float" }
+              end
+            end,
+            desc = "ToggleTerm spotify",
+          },
+        },
+      },
+    },
+  },
 }
