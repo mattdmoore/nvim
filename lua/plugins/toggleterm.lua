5997d84d930229e7c6884a314b3d55420ccfd598
Matthew Moore <matthew.moore@orbem.ai>
2024-08-05 09:09:53 +0200

    Fix OS-specific shell

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,13 @@
+local shell
+if jit.os == "Linux" then
+  shell = "fish"
+else
+  shell = "zsh"
+end
+
 return {
   "akinsho/toggleterm.nvim",
   opts = {
-    shell = "fish",
+    shell = shell,
   },
 }
